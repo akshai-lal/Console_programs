e6d55c1b5f9d8c9f1cc46e0036644afb80668408
akshai-lal <akshai.lal@neudesic.com>
2025-09-23 12:41:22 +0530

    first commit

---
+++ ConsolePrograms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace program
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, Akshai! Welcome to C# programming.");
+        }
+    }
+}
